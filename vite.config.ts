9f107916c1280454657e15c8891e46b6510d1a51
Joao Vitor Reck Gimenes <joao.invoke@hotmail.com>
2026-09-24 20:45:01 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => {
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(import.meta.dirname, '.'), // ✨ Atualizado para suportar o configLoader nativo
       },
     },
     server: {
